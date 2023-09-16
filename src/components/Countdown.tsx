e85a5515a814a5b3f8130179a62c9a73e9964503
navoren <namantomar1453@gmail.com>
2023-09-16 16:59:13 +0530

    Added Timer Countdown

---
+++ src/components/Countdown.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+interface ICountdown {
+    days: number;
+    hours: number;
+    minutes: number;
+    seconds: number;
+}
+
+const CountDownTimer = ({ days=0, hours = 0, minutes = 0, seconds = 60 }: ICountdown) => {
+    
+
+    const [time, setTime] = React.useState<ICountdown>({days, hours, minutes, seconds});
+    
+
+    const tick = () => {
+
+        if (time.days ===0 && time.hours === 0 && time.minutes === 0 && time.seconds === 0)
+            reset()
+            else if (time.hours === 0 && time.minutes === 0 && time.seconds === 0) {
+                setTime({ days: time.days - 1, hours: 23, minutes: 59, seconds: 59 });
+            } else if (time.minutes === 0 && time.seconds === 0) {
+                setTime({ days: time.days, hours: time.hours - 1, minutes: 59, seconds: 59 });
+            } else if (time.seconds === 0) {
+                setTime({ days: time.days, hours: time.hours, minutes: time.minutes - 1, seconds: 59 });
+            } else {
+                setTime({ days: time.days, hours: time.hours, minutes: time.minutes, seconds: time.seconds - 1 });
+            }
+            };
+
+
+    const reset = () => setTime({days: time.days, hours: time.hours, minutes: time.minutes, seconds: time.seconds});
+
+    
+    React.useEffect(() => {
+        const timerId = setInterval(() => tick(), 1000);
+        return () => clearInterval(timerId);
+    });
+
+    
+    return (
+            <div className='wrapper' style={{backgroundColor: 'green'}}>
+            <center>
+            <h1  style={{fontSize: '50px'}}>Time until SIH:</h1>
+            <div className='custom-fontsize'>{`${time.days.toString().padStart(2, '0')}:${time.hours.toString().padStart(2, '0')}:${time.minutes
+        .toString()
+        .padStart(2, '0')}:${time.seconds.toString().padStart(2, '0')}`}</div>
+            
+            </center>
+        </div>
+        
+    );
+}
+
+export default CountDownTimer;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,12 @@
+import CountDownTimer from "../components/Countdown"
 const Home = () => {
     return (
-        <div>Home</div>
+        <div className="wrapper">
+            <div>
+            <CountDownTimer days={1} hours={1} minutes={20} seconds={40}/>
+            </div>
+
+        </div>
     )
 }
 
