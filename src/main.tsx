389c1d89f9559ce63809bd77fa0fb27a4e599cee
anant211205 <anant211205@gmail.com>
2023-09-18 01:18:23 +0530

    about

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,19 @@ import ReactDOM from 'react-dom/client'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Guidelines from './components/Guide_lines';
+import About from './components/About';
 
 //CSS
 import './CSS/index.css'
 import './CSS/Guide_lines.css'
 import './CSS/HeaderFooter.css'
+import './CSS/About.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Navbar/>
     <Guidelines />
+    <About/>
     <Footer/>
   </React.StrictMode>,
 )
