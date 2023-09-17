1c11c8ea596c37f40a898405626c2dd650ec6125
SrishtiSonam <113123593+SrishtiSonam@users.noreply.github.com>
2023-09-17 09:38:04 +0530

    Guidelines First

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import './Guide_lines.css'
+import Guidelines from './components/Guide_lines';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <h1>Hello</h1>
   </React.StrictMode>,
 )
+ReactDOM.createRoot(document.getElementById('guide')!).render(
+  <React.StrictMode>
+    <Guidelines />
+  </React.StrictMode>,
+);
